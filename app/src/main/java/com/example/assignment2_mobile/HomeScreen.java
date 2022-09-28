b44d80a5ac0614675fbc9e5ea349d72eb2f66d1c
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-09-28 21:15:42 +0300

    Created methods to add name on the shared preferences

--- app/src/main/java/com/example/assignment2_mobile/HomeScreen.java
+++ app/src/main/java/com/example/assignment2_mobile/HomeScreen.java
@@ -2,15 +2,40 @@ package com.example.assignment2_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeScreen extends AppCompatActivity {
-
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+    }
+    public void go(View v){
+        EditText name= (EditText) findViewById(R.id.name);
+        String name_val=name.getText().toString();
+        if(!isNameSaved()){
+            saveName(name);
+        }
+        Intent i= new Intent(getApplicationContext(),FetchNews.class);
+        i.putExtra("name",name_val);
+        startActivity(i);
+    }
+    public void saveName(EditText text) {
+        SharedPreferences sharedPref = getSharedPreferences("application", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("NAME", text.getText().toString());
+        editor.apply();
+    }
+    public boolean isNameSaved() {
+        SharedPreferences sharedPref = getSharedPreferences("application", Context.MODE_PRIVATE);
+        return sharedPref.contains("NAME");
     }
 }
