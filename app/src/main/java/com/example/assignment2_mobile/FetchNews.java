ac7522344735120f8acd0d0d9b95bb1fafef5bc9
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-09-28 22:45:25 +0300

    Created the listview for the news feed

--- app/src/main/java/com/example/assignment2_mobile/FetchNews.java
+++ app/src/main/java/com/example/assignment2_mobile/FetchNews.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class FetchNews extends AppCompatActivity {
 
+    ListView my_list;
+    ArrayList<String> the_list;
+    ArrayAdapter<String>adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,8 @@ public class FetchNews extends AppCompatActivity {
         brew.setX(1000);
         brew.animate().translationX(-450).setDuration(2000);
 
+        my_list= findViewById(R.id.my_list);
+        the_list=new ArrayList<String>();
 
         try{
 
@@ -38,11 +49,22 @@ public class FetchNews extends AppCompatActivity {
 
             while(c!= null){
                 String news_name = c.getString(n_nameIndex) + " ";
-                Toast.makeText(getApplicationContext(), news_name, Toast.LENGTH_LONG).show();
+                the_list.add(news_name);
                 c.moveToNext();
             }
         }catch(Exception e){
             e.printStackTrace();
         }
+
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,the_list);
+        my_list.setAdapter(adapter);
+        my_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent= new Intent(getApplicationContext(),NewsDetails.class);
+                intent.putExtra("news_name",the_list.get(i));
+                startActivity(intent);
+            }
+        });
     }
 }
