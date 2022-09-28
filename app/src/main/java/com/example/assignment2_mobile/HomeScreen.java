76a11e20d273295ac3dc937614fa1d4447b09ebb
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-09-28 20:02:09 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/example/assignment2_mobile/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.assignment2_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+    }
+}
--- app/src/main/java/com/example/assignment2_mobile/MainActivity.java
+++ app/src/main/java/com/example/assignment2_mobile/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.assignment2_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
     }
 }
